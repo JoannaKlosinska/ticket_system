335a86b556fe5a9d828088a5187e630b0c27a941
Joanna Klosinska <joanna.klosinska.55@gmail.com>
2021-01-26 09:23:23 +0100

    Config Jason Web Token

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,9 +1,9 @@
 class JsonWebToken
-  SECRET_KEY = Rails.application.secrets.secret_key_base. to_s
+  SECRET_KEY = Rails.application.secrets.secret_key_base
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, HMAC_SECRET)
+    JWT.encode(payload, SECRET_KEY)
   end
 
   def self.decode(token)
